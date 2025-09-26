699398679497489cce0f55fd2a78e064d887ef19
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-09-26 19:48:56 -0300

    react_tabs-with-router task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,122 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import {
+  Routes,
+  Route,
+  Link,
+  useParams,
+  Outlet,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">
-            Home
-          </a>
-          <a href="/tabs" className="navbar-item">
-            Tabs
-          </a>
-        </div>
-      </div>
-    </nav>
+const Home = () => (
+  <section className="section">
+    <div className="container">
+      <h1 className="title">Home page</h1>
+    </div>
+  </section>
+);
 
+const Tabs = () => {
+  const { tabId } = useParams();
+
+  return (
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
         <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
 
         <div className="tabs is-boxed">
           <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                data-cy="Tab"
+                className={tabId === tab.id ? 'is-active' : ''}
+              >
+                <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              </li>
+            ))}
           </ul>
         </div>
 
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+        <Outlet />
       </div>
     </div>
-  </>
+  );
+};
+
+const TabSelect = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  if (!selectedTab) {
+    return (
+      <div className="block" data-cy="TabContent">
+        Please select a tab
+      </div>
+    );
+  }
+
+  return (
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  );
+};
+
+const Erro = () => (
+  <div>
+    <h1 className="title">Page not found</h1>
+  </div>
 );
+
+export const App = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={`navbar-item ${pathname === '/' ? 'is-active' : ''}`}
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={`navbar-item ${pathname.startsWith('/tabs') ? 'is-active' : ''}`}
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div>
+        <Routes>
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/tabs" element={<Tabs />}>
+            <Route index element={<TabSelect />} />
+            <Route path=":tabId" element={<TabSelect />} />
+          </Route>
+          <Route path="*" element={<Erro />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
